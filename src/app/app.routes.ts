99037c32180c96c772437459dc2da71023bb85c2
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-05-02 17:21:15 +0300

    ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'app',  // 'home' path'ine yönlendir
+    pathMatch: 'full'    // sadece tam eşleşmede yönlendir
+  },
+  {
+    path: 'home',
+    component: AppComponent
+  }
+];
