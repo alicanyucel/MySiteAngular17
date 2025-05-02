56b7a6e01b99ac40347445b0789307dbb3eb271f
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-05-02 15:29:47 +0300

    tamam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectComponent } from './project/project.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,SkillsComponent,HeaderComponent,RouterOutlet,ProjectComponent,ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'Ali Can Yücel Kişisel Web Sayfası';
 }
