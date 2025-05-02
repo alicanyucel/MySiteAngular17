a6b60b3e06792f54d33262de794e1b31b1397243
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-05-02 15:42:57 +0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './header/header.component';
 import { ProjectComponent } from './project/project.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,SkillsComponent,HeaderComponent,RouterOutlet,ProjectComponent,ExperienceComponent],
+  imports: [CommonModule,SkillsComponent,HeaderComponent,RouterOutlet,FooterComponent,ProjectComponent,ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
